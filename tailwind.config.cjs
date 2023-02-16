2e4fec7010db539e1961cec65ca8937e34e6f876
Garrett Welson <garrettwelson@gmail.com>
2023-02-15 23:26:50 -0800

    Remove need for main CSS file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Playfair Display', 'serif'],
+		},
+		colors: {
+			header: '#9bd7db'
+		},
 		extend: {},
 	},
 	plugins: [],
