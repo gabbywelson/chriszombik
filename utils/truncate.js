3d49d7e4f2279b977e6b7ab8d9a9fa876161fec2
Garrett Welson <garrettwelson@gmail.com>
2023-02-16 21:21:07 -0800

    Fix ellipsis

--- utils/truncate.js
+++ utils/truncate.js
@@ -1,6 +1,6 @@
 export default function truncate(str) {
     const shortened = str.substring(0,600)
-    const lastPeriodIdx = shortened.lastIndexOf(".") + 1
+    const lastPeriodIdx = shortened.lastIndexOf(".")
     const truncated = shortened.substring(0, lastPeriodIdx)
     return truncated
 }
