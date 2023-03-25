d5ec4b0f32e9fefdfbc985a813e074ccff88fa23
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-03-25 00:13:45 -0700

    More debugging

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    readonly TINA_CLIENT_ID: string
-    readonly TINA_CLIENT_TOKEN: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
