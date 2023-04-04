7bf349d9bdd8e58ccb58ab164b89e3642cbfd7d1
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-04-03 18:28:57 -0700

    Add content schema

---
+++ src/content/config.ts
@@ -0,0 +1,40 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.string(),
+    draft: z.boolean(),
+    tags: z.array(z.string()).optional(),
+    coverImage: z.string().optional(),
+    coverImageAlt: z.string().optional()
+  }),
+});
+
+const staticSchema = defineCollection({
+  schema: z.object({
+    title: z.string()
+  })
+});
+
+const worksSchema = defineCollection({
+  schema: z.object({
+    title: z.string()
+  })
+});
+
+const upcomingSchema = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    status: z.string(),
+    pinned: z.boolean()
+  })
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'posts': blogCollection,
+  'static': staticSchema,
+  'works': worksSchema,
+  'upcoming': upcomingSchema
+};
