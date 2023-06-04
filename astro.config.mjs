44a7eea54d31ee69b5ee9ab4277d39dce557959d
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-06-04 09:51:11 -0700

    Add sitemap addon and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import remarkGfm from "remark-gfm"
+import remarkGfm from "remark-gfm";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://chriszombik.com',
+  integrations: [tailwind(), sitemap()],
   markdown: {
     // Applied to .md and .mdx files
     remarkPlugins: [remarkGfm],
-    drafts: true,
-  },
+    drafts: true
+  }
 });
